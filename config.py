87d0c98f0496a797abd34f7c79c369a830fbe6c6
tony0392 <54049750+tony0392@users.noreply.github.com>
2024-12-02 15:04:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -52,10 +52,13 @@ url_blacklist = [
 ]
 
 announcements = [
-    {
+     {
         "channel": "公告",
         "entries": [
-            {"name": "张俊峰个人项目", "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E4%B8%BB%E8%A7%92.mp4", "logo": "http://175.178.251.183:6689/LR.jpg"},
+            {"name": "张俊峰", "url": "https://liuliuliu.tv/api/channels/1997/stream", "logo": "http://175.178.251.183:6689/LR.jpg"},
+            {"name": "个人项目", "url": "https://liuliuliu.tv/api/channels/233/stream", "logo": "http://175.178.251.183:6689/LR.jpg"},
+            {"name": "非商业化", "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E4%B8%BB%E8%A7%92.mp4", "logo": "http://175.178.251.183:6689/LR.jpg"},
+            {"name": None, "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E8%B5%B7%E9%A3%8E%E4%BA%86.mp4", "logo": "http://175.178.251.183:6689/LR.jpg"}
         ]
     }
 ]
