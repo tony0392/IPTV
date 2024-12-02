8b416a52bac6562c98df0f5a4946cefc121758f9
tony0392 <54049750+tony0392@users.noreply.github.com>
2024-12-02 14:04:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,26 @@
 ip_version_priority = "ipv6"
 
 source_urls = [
-    "https://raw.githubusercontent.com/lizanyang3/lizanyang3.github.io/refs/heads/main/hn.m3u"，    
-    "http://175.178.251.183:6689/aktvlive.txt",
+   "https://raw.githubusercontent.com/lizanyang3/lizanyang3.github.io/refs/heads/main/hn.m3u"，    
+    "https://www.mytvsuper.xyz/m3u/Live.m3u",
+    "https://raw.githubusercontent.com/HerbertHe/iptv-sources/refs/heads/gh-pages/all.m3u",
+    "http://175.178.251.183:6689/channel.txt",
+    "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
     "http://120.79.4.185/new/mdlive.txt",
     "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
-    "https://live.zhoujie218.top/tv/iptv4.txt",
-    "https://www.mytvsuper.xyz/m3u/Live.m3u",
+    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
+    "https://live.zhoujie218.top/tv/iptv6.txt",
     "https://tv.youdu.fan:666/live/",
     "http://ww.weidonglong.com/dsj.txt",
     "http://xhztv.top/zbc.txt",
+    "https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt",
     "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
-    "https://raw.githubusercontent.com/Guovin/TV/gd/output/result.txt",
+    "https://v.nxog.top/m/tv/1/",
+    "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
     "http://home.jundie.top:81/Cat/tv/live.txt",
     "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
     "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
-    "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
-    "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
-    "https://fm1077.serv00.net/SmartTV.m3u",
-    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
-    "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
 ]
 announcements = [
    
