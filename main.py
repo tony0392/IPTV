043822f43b815018e9b5ac754831eb8b173b3493
tony0392 <54049750+tony0392@users.noreply.github.com>
2024-12-02 14:29:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ def updateChannelUrlsM3U(channels, template_channels):
 
                             total_urls = len(filtered_urls)
                             for index, url in enumerate(filtered_urls, start=1):
-                                  if is_ipv6(url):
+                                if is_ipv6(url):
                                     url_suffix = f"$LR•IPV6" if total_urls == 1 else f"$LR•IPV6『线路{index}』"
                                 else:
                                     url_suffix = f"$LR•IPV4" if total_urls == 1 else f"$LR•IPV4『线路{index}』"
@@ -151,7 +151,7 @@ def updateChannelUrlsM3U(channels, template_channels):
 
                                 new_url = f"{base_url}{url_suffix}"
 
-                                f_m3u.write(f"#EXTINF:-1 tvg-id=\"{index}\" tvg-name=\"{channel_name}\" tvg-logo=\"https://gitee.com/yuanzl77/TVBox-logo/raw/main/png/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n")
+                                f_m3u.write(f"#EXTINF:-1 tvg-id=\"{index}\" tvg-name=\"{channel_name}\" tvg-logo=\"https://gcore.jsdelivr.net/gh/yuanzl77/TVlogo@master/png/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n")
                                 f_m3u.write(new_url + "\n")
                                 f_txt.write(f"{channel_name},{new_url}\n")
 
