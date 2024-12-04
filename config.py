0e7c3e80725b49db1b1d40c93f819e2fae394361
tony0392 <54049750+tony0392@users.noreply.github.com>
2024-12-04 15:26:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,6 @@ source_urls = [
     "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
     "http://home.jundie.top:81/Cat/tv/live.txt",
     "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
-    "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
     "https://fm1077.serv00.net/SmartTV.m3u",
     "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
 ]
